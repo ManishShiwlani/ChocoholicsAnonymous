dc1b35e90626ccb2898b75f311bf0ad718fe32e2
manish <manishshiwlani123@gmail.com>
2020-05-02 23:46:44 -0400

    Initial commit

---
+++ provider/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Provider(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zip_code = models.IntegerField()
+
